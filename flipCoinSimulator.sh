8a138a3f8cf3d12116c8999633886a44d6ba168e
“sandip5” <singhsandeep.singh97@gmail.com>
2020-05-03 12:27:24 +0530

    UC four winning difference

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,16 +3,17 @@
 isHead=0;
 isHeadWon=0;
 isTailWon=0;
+winDifference=0;
 
 while [[ $isHeadWon -lt 21 && $isTailWon -lt 21 ]]
 do
 randomCheck=$(( RANDOM%2 ))
 if [[ isHead -eq randomCheck ]]
 then
-	echo "Tail"
+	echo "Head"
 	isHeadWon=$(( $isHeadWon + 1 ))
 else
-	echo "Head"
+	echo "Tail"
 	isTailWon=$(( $isTailWon + 1 ))
 fi
 done
@@ -23,11 +24,30 @@ echo "Total number of tail win is $isTailWon"
 if [[ isHeadWon -lt isTailWon ]]
 then
 	echo "Tail Win"
-elif [[ isHeadWon -eq isTailWon ]]
-then
-	echo "Tie"
+	#winDifference=$(( $isTailWon - $isHeadWon ))
 else
 	echo "Head Win"
+	#winDifference=$(( $isHeadWon - $isTailWon ))
+fi
+
+if [[ isHeadWon -eq isTailWon ]]
+then
+while [[ $winDifference -ge 2 ]]
+do
+randomCheck=$(( RANDOM%2 ))
+if [[ isHead -eq randomCheck ]]
+then
+        echo "Head"
+        isHeadWon=$(( $isHeadWon + 1 ))
+	winDifference=$(( $isHeadWon - $isTailWon ))
+else
+        echo "Tail"
+        isTailWon=$(( $isTailWon + 1 ))
+	winDifference=$(( $isTailWon - $isHeadWon ))
+fi
+done
+else
+	echo "No Tie"
 fi
 
 
